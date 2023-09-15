2af88bd14819b330c490da4763441b51682d6b6f
gabriel santos Vieira <GabrielsantosV2@hotmail.com>
2023-09-14 21:43:24 -0300

    Implementação das controllers.

--- Robotron/Controllers/AulaController.cs
+++ Robotron/Controllers/AulaController.cs
@@ -1,6 +1,31 @@
-﻿namespace Robotron.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Robotron.Model;
+using Robotron.Repository.Carro;
+using Robotron.Repository.Computador;
+
+namespace Robotron.Controllers
 {
-    public class AulaController
+    [Route("api/v1/aula")]
+    [ApiController]
+    public class AulaController : ControllerBase
     {
+
+        [Route("alunos")]
+        [HttpGet]
+        public ActionResult ListAlunos() {
+
+            List<Aluno> alunos = AlunoRepos.lisAluno();
+            return Ok(alunos);
+        }
+        [Route("professores")]
+        [HttpGet]
+        public ActionResult ListProfessores()
+        {
+
+            List<Professor> professores = ProfessorRepos.listProfessores();
+            return Ok(professores);
+        }
+
     }
+ 
 }
--- Robotron/Repository/Aluno/AlunoRepos.cs
+++ Robotron/Repository/Aluno/AlunoRepos.cs
@@ -1,4 +1,5 @@
 ﻿using Robotron.Model;
+using Robotron.Repository.Computador;
 
 namespace Robotron.Repository.Carro
 {
@@ -7,39 +8,46 @@ namespace Robotron.Repository.Carro
         public static List<Aluno> lisAluno()
         {
             List<Aluno> alunos = new List<Aluno>();
-            
-            alunos.Add(new Aluno {
+
+            alunos.Add(new Aluno
+            {
                 Id = 1,
                 Nome = "Gabriel dos Santos Vieira",
-                Idade = 22
-                });
+                Idade = 22,
+                Professores = ProfessorRepos.listProfessores().Where(x => x.Curso == "Química").ToList()
+            });
 
             alunos.Add(new Aluno
             {
                 Id = 2,
                 Nome = "João messi souza",
-                Idade = 22
+                Idade = 22,
+                Professores = ProfessorRepos.listProfessores().Where(x => x.Curso == "Matemática").ToList()
+
             });
 
             alunos.Add(new Aluno
             {
                 Id = 3,
                 Nome = "Cristiano ronaldo",
-                Idade = 3
+                Professores = ProfessorRepos.listProfessores().Where(x => x.Curso == "Física").ToList()
             });
 
             alunos.Add(new Aluno
             {
                 Id = 4,
                 Nome = "Luva de pedreiro",
-                Idade = 20
+                Idade = 20,
+                Professores = ProfessorRepos.listProfessores().Where(x => x.Curso == "Matemática").ToList()
+
             });
 
             alunos.Add(new Aluno
             {
                 Id = 5,
                 Nome = "Alan Jesus",
-                Idade = 36
+                Idade = 36,
+                Professores = ProfessorRepos.listProfessores().Where(x => x.Curso == "Química").ToList()
             });
 
             return alunos;
